c31e7c07b801d7420c6a62ca815234b8f82f2352
Stanley Zapata <stanley@theradical1s.com>
2024-02-13 11:24:04 -0400

    Adds LWC logic to show movies on markup

--- force-app/main/default/lwc/movieCloud/movieCloud.js
+++ force-app/main/default/lwc/movieCloud/movieCloud.js
@@ -1,3 +1,52 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import getMovies from "@salesforce/apex/MovieCloudController.getMovies";
+import BASE_IMAGE_URL from "@salesforce/label/c.tmdb_base_image_url";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class MovieCloud extends LightningElement {}
+export default class MovieCloud extends LightningElement {''
+  currentPage = 1;
+  totalPage = 0;
+  totalResults = 0;
+  movies = [];
+  errorMessage = null;
+  label = {
+    BASE_IMAGE_URL
+  };
+
+  async connectedCallback() {
+    await this.loadMovies();
+  }
+
+  async loadMovies() {
+    try {
+      const response = await getMovies({
+        "include_adult": false,
+        "include_video": false,
+        "language": "en-US",
+        "page": this.currentPage,
+        "sort_by": "vote_average.desc",
+        "without_genres": "99,10755",
+        "vote_count.gte": 200
+      });
+      response.results = response.results.map(movie => {
+        movie.poster_path = this.label.BASE_IMAGE_URL + movie.poster_path;
+        movie.backdrop_path = this.label.BASE_IMAGE_URL + movie.backdrop_path;
+        return movie;
+      });
+      this.movies = [...this.movies, ...response.results];
+      this.totalPage = response.total_pages;
+      this.totalResults = response.total_results;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  handleError(error) {
+    this.errorMessage = error.body.message || error.message || JSON.stringify(error);
+    this.dispatchEvent(new ShowToastEvent({
+      title: "Error",
+      message: this.errorMessage,
+      variant: "error"
+    }));
+  }
+}
